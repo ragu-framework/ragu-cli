ac83ed3785dceee953090c4a695b84ceeadb1c6c
Maniero <carlosmaniero@gmail.com>
2021-03-26 23:53:58 -0300

    adds bg param to load mfe with background

--- onboarding-mfe/mfe.jsx
+++ onboarding-mfe/mfe.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import App from "./src/App";
-import {createGlobalStyle} from "styled-components";
+import styled, {createGlobalStyle} from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
-    background: linear-gradient(122deg, rgba(191,38,94,1) 0%, rgb(255,171,64) 100%);
-    background-repeat: no-repeat;
-    background-attachment: fixed;
   }
 `
 
-const RenderContent = ({env, children}) => {
-  if(env === 'dev') {
+const ColorBackground = styled.div`
+  background: linear-gradient(122deg, rgba(191,38,94,1) 0%, rgb(255,171,64) 100%);
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+`
+
+const RenderContent = ({params, children}) => {
+  if(params.env === 'dev') {
     return <div>
       <GlobalStyle />
       <link
         href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300&family=Poppins:wght@300;500&display=swap&family=Source+Code+Pro:wght@1;300"
         rel="stylesheet" />
-      {children}
+      <ColorBackground>{children}</ColorBackground>
     </div>
   }
 
-  return children
+  if (params.bg) {
+    return <ColorBackground>{children}</ColorBackground>
+  }
+
+  return children;
 }
 
-export default ({env}) => <RenderContent env={env}>
+export default ({params}) => <RenderContent params={params}>
   <App />
 </RenderContent>
